1aabe58a7a5fcd2be1721ae7c88f88dfbc89a45b
Ryan Kois <ryan.kois@gmail.com>
2016-01-02 22:14:07 -0800

    Remove action constants. Use literals.

--- client/reducers/counter.js
+++ client/reducers/counter.js
@@ -1,9 +1,8 @@
 import { handleActions } from 'redux-actions'
-import { COUNTER_INCREMENT } from '../constants'
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
 export default handleActions({
-  [COUNTER_INCREMENT]: (state, { payload }) => state + payload
+  ['increment counter']: (state, { payload }) => state + payload
 }, 1)
