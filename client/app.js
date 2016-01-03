e34aa2d0ed97d5291da081d8f0870ab32133a4ea
contra <contra@maricopa.edu>
2016-01-02 21:38:28 -0800

    Revert "upgrade react-router"

--- client/app.js
+++ client/app.js
@@ -1,17 +1,18 @@
 import ReactDOM from 'react-dom'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 import { syncReduxAndRouter } from 'redux-simple-router'
-import { browserHistory } from 'react-router'
 import routes from './routes'
 import Root from './containers/Root'
 import configureStore from './redux/configureStore'
 import React from 'react-native-web'
 
+const history = createBrowserHistory()
 const store = configureStore(window.__INITIAL_STATE__)
 
-syncReduxAndRouter(browserHistory, store, (state) => state.router)
+syncReduxAndRouter(history, store, (state) => state.router)
 
 // Render the React application to the DOM
 ReactDOM.render(
-  <Root history={browserHistory} routes={routes} store={store} />,
+  <Root history={history} routes={routes} store={store} />,
   document.getElementById('root')
 )
