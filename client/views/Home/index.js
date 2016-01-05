b46877368342d3671cec61c6cfa9b11aebf4ae74
Contra <contra@maricopa.edu>
2016-01-04 20:17:54 -0800

    more gh data, change api action creator

--- client/views/Home/index.js
+++ client/views/Home/index.js
@@ -8,10 +8,21 @@ import style from './style.scss'
 import actions from 'actions'
 
 export class HomeView extends Component {
+  getData (name) {
+    var opt = { user: name }
+    this.getActions().getOrganizations({options: opt})
+    this.getActions().getRepositories({options: opt})
+    this.getActions().getUser({options: opt})
+  }
+
   render () {
-    let store = this.getStore()
-    let actions = this.getActions()
-    let orgs = this.getCollection('organization')
+    var name = 'funkytek'
+    var store = this.getStore()
+    var actions = this.getActions()
+    var orgs = this.getCollection('organization')
+    var repos = this.getCollection('repository')
+    var user = this.getCollection('user').filter(({login}) => login === name)[0]
+    var fetching = !orgs.length || !repos.length || !user
 
     return (
       <div className={style.home}>
@@ -34,18 +45,39 @@ export class HomeView extends Component {
           <button onClick={shield(actions.zero)} className={style.actionButton}>
             Zero (Hose)
           </button>
-          <button onClick={shield(actions.getOrgs)} className={style.actionButton}>
-            Get Orgs
+          <button onClick={this.getData.bind(this, name)} className={style.actionButton}>
+            Get GH Data
           </button>
         </div>
-        <ul className={style.repoList}>
-          <div>{orgs.length} repositories</div>
+        <div className={style.githubData}>
           {
-            orgs.map((org) =>
-              <li className={style.repo} key={org.id}>{org.login}</li>
+            fetching ? (<Title>No GH Data</Title>) : (
+              <div>
+                <div className={style.list}>
+                  <Title>User Info</Title>
+                  <img src={user.avatar_url} className={style.userImage}/>
+                  <div className={style.listItem}>{user.name}</div>
+                </div>
+                <ul className={style.list}>
+                  <Title>{orgs.length} organizations</Title>
+                  {
+                    orgs.map((org) =>
+                      <li className={style.listItem} key={org.id}>{org.login}</li>
+                    )
+                  }
+                </ul>
+                <ul className={style.list}>
+                  <Title>{repos.length} repositories</Title>
+                  {
+                    repos.map((repo) =>
+                      <li className={style.listItem} key={repo.id}>{repo.full_name} - Issues: {repo.open_issues}</li>
+                    )
+                  }
+                </ul>
+              </div>
             )
           }
-        </ul>
+        </div>
         <Link to='/about'>Go To About View</Link>
       </div>
     )
