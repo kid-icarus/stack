191041d1109d9ba6eb82639220b4c46a5f65c124
funkytek <aaron@wearefractal.com>
2016-01-06 02:35:30 -0800

    save todo

--- client/actions/todos.js
+++ client/actions/todos.js
@@ -3,3 +3,4 @@ import { createAction } from 'redux-actions'
 export const addTodo = createAction('addTodo')
 export const deleteTodo = createAction('deleteTodo')
 export const toggleTodo = createAction('toggleTodo')
+export const saveTodo = createAction('saveTodo')
