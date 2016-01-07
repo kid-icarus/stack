0f889722f9649839eecc0cc19289468795b40745
Contra <contra@maricopa.edu>
2016-01-06 19:01:57 -0800

    clean up reducer files

--- client/reducers/counter.js
+++ client/reducers/counter.js
@@ -2,10 +2,7 @@ import { handleActions } from 'redux-actions'
 
 const initialState = 1
 
-const incrementCounter = (state, { payload = 1 }) => state + payload
-const decrementCounter = (state, { payload = 1 }) => state - payload
+export const incrementCounter = (state, { payload = 1 }) => state + payload
+export const decrementCounter = (state, { payload = 1 }) => state - payload
 
-export default handleActions({
-  incrementCounter,
-  decrementCounter
-}, initialState)
+export default handleActions(exports, initialState)
