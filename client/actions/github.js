43f58849c9dfec447dc05418cf6c4668b05c1dc5
Contra <contra@maricopa.edu>
2016-01-08 08:48:45 -0800

    normalize.css

--- client/actions/github.js
+++ client/actions/github.js
@@ -5,20 +5,20 @@ const user = new Schema('user')
 const organization = new Schema('organization')
 const repository = new Schema('repository')
 
-export const getUser = createAPIAction('getUser', {
+export const getUser = createAPIAction({
   endpoint: ({user}) => `https://api.github.com/users/${user}`,
   method: 'GET',
   model: user
 })
 
-export const getOrganizations = createAPIAction('getOrganizations', {
+export const getOrganizations = createAPIAction({
   endpoint: ({user}) => `https://api.github.com/users/${user}/orgs`,
   method: 'GET',
   collection: true,
   model: organization
 })
 
-export const getRepositories = createAPIAction('getRepositories', {
+export const getRepositories = createAPIAction({
   endpoint: ({user}) => `https://api.github.com/users/${user}/repos`,
   method: 'GET',
   collection: true,
