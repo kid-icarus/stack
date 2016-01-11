31738996c2787cc11d310b38e2f2bee2f7b05838
funkytek <aaron@wearefractal.com>
2016-01-10 17:48:03 -0800

    cleanup

--- client/views/CRM/index.js
+++ client/views/CRM/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 // import Icon from 'react-icon'
 // import shield from 'function-shield'
 import Component from 'redux-dgaf'
-import classes from './index.sass'
 import {Button, Search} from 'react-semantify'
 
 export class CRMView extends Component {
