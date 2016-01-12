bee41386e332e74456371f3d2d36e1351d372bd5
Contra <contra@maricopa.edu>
2016-01-11 16:48:53 -0800

    split webpack apart

--- server/http/express/index.js
+++ server/http/express/index.js
@@ -11,15 +11,7 @@ app.use(historyApiFallback({
 
 // Serve app with Webpack if HMR is enabled
 if (config.compiler_enable_hmr) {
-  const webpack = require('webpack')
-  const webpackConfig = require('../../../build/webpack')
-  const compiler = webpack(webpackConfig)
-
-  app.use(require('./middleware/webpack-dev')({
-    compiler,
-    publicPath: webpackConfig.output.publicPath
-  }))
-  app.use(require('./middleware/webpack-hmr')({ compiler }))
+  app.use(require('./middleware/webpack'))
 } else {
   app.use(express.static(paths.base(config.dir_dist)))
 }
