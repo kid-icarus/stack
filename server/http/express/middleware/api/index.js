3b5d6a591821cc8e8085ff2054d362cc11fc41a9
Contra <contra@maricopa.edu>
2016-01-12 22:24:33 -0800

    add api resource handler sugar, #10

--- server/http/express/middleware/api/index.js
+++ server/http/express/middleware/api/index.js
@@ -1,23 +1,27 @@
 // import {compose} from 'compose-middleware'
+import {Router} from 'express'
 import path from 'path'
 import config from 'app-config-chain'
 import loadResources from './loadResources'
+import wrapHandler from './wrapHandler'
 import _debug from 'debug'
 const debug = _debug('app:api:loader')
 
+// load in the resources
 const resources = loadResources({
   prefix: config.api.path,
   path: path.join(config.paths.base, './server/resources')
 })
 
-if (config.env !== 'production') {
-  Object.keys(resources).forEach((resourceName) => {
-    var endpoints = resources[resourceName]
-    debug(`Loaded ${endpoints.length} endpoints for "${resourceName}"`)
-    endpoints.forEach((endpoint) =>
-      debug(`  - ${endpoint.name} (${endpoint.method.toUpperCase()} ${endpoint.path})`)
-    )
+// construct the router
+const router = Router({mergeParams: true})
+Object.keys(resources).forEach((resourceName) => {
+  var endpoints = resources[resourceName]
+  debug(`Loaded ${endpoints.length} endpoints for "${resourceName}"`)
+  endpoints.forEach((endpoint) => {
+    debug(`  - ${endpoint.name} (${endpoint.method.toUpperCase()} ${endpoint.path})`)
+    router[endpoint.method](endpoint.path, wrapHandler(endpoint.handler))
   })
-}
+})
 
-export default (req, res, next) => next()
+export default router
--- server/resources/user/find.js
+++ server/resources/user/find.js
@@ -1,3 +1,12 @@
 // TODO
 
-export default () => {}
+export default (opt, cb) => {
+  if (!opt.user) {
+    return cb({
+      status: 403,
+      error: 'Not logged in'
+    })
+  }
+
+  cb(null, {test: 123})
+}
