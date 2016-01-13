d1470e82ae8d619de030c43d82dcb75fab71f0a3
Contra <contra@maricopa.edu>
2016-01-12 22:08:11 -0800

    debug loaded api endpoints, #10

--- server/http/express/middleware/api/index.js
+++ server/http/express/middleware/api/index.js
@@ -2,11 +2,22 @@
 import path from 'path'
 import config from 'app-config-chain'
 import loadResources from './loadResources'
+import _debug from 'debug'
+const debug = _debug('app:api:loader')
 
-const endpoints = loadResources({
+const resources = loadResources({
   prefix: config.api.path,
   path: path.join(config.paths.base, './server/resources')
 })
-// console.log(endpoints)
+
+if (config.env !== 'production') {
+  Object.keys(resources).forEach((resourceName) => {
+    var endpoints = resources[resourceName]
+    debug(`Loaded ${endpoints.length} endpoints for "${resourceName}"`)
+    endpoints.forEach((endpoint) =>
+      debug(`  - ${endpoint.name} (${endpoint.method.toUpperCase()} ${endpoint.path})`)
+    )
+  })
+}
 
 export default (req, res, next) => next()
