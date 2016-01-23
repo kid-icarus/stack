7a6b65fa3b07e6ea259b514b4f572453ea1b3a10
funkytek <aaron@wearefractal.com>
2016-01-23 21:03:08 +0900

    reduxMountPoint

--- client/core/reducers.js
+++ client/core/reducers.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'shasta'
 import { reducer as routeReducer } from 'shasta-router'
 import { reducers as apiReducers } from 'tahoe'
 import { reducer as formReducer } from 'shasta-forms'
-import toReducers from 'modules-to-reducers'
+import { toReducers } from 'modules-to-redux'
 import localReducers from 'reducers/.lookup'
 
-export default combineReducers({
+export const reducers = {
   ...apiReducers,
   ...toReducers(localReducers),
   router: routeReducer,
   forms: formReducer
-})
+}
+
+export default combineReducers(reducers)
