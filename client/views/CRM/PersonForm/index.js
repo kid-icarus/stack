4cdab35e2b477b0f36f6a73a7ec429030b10b6ae
funkytek <aaron@wearefractal.com>
2016-01-25 23:28:24 +0900

    good start at form api

--- client/views/CRM/PersonForm/index.js
+++ client/views/CRM/PersonForm/index.js
@@ -1,93 +1,58 @@
 import React from 'react'
 import { PropTypes, Component } from 'shasta'
-import { Form, FormComponent } from 'shasta-forms'
-// export const fields = ['name', 'location', 'smallImage', 'largeImage', 'email', 'twitter', 'facebook', 'instagram', 'id']
-
+import { Form, Field, shastaForm } from 'shasta-forms'
 // import { fields, validate } from './schema'
-import classNames from 'classnames'
-
-import schema from './schema'
+// import schema from './schema'
 import buildSchema from 'redux-form-schema'
+
+// Schema - define form fields
+let schema = {
+  name: {
+    label: 'Name',
+    required: true,
+    validate: { length: {min: 0, max: 5} }
+  },
+  location:   {label: 'Location', required: true},
+  smallImage: {label: '', required: true},
+  largeImage: {label: '', required: true},
+  email:      {label: 'Email', required: true, type: 'email'},
+  twitter:    {label: 'Twitter', required: true},
+  facebook:   {label: 'Facebook', required: true},
+  instagram:  {label: 'Instagram', required: true}
+}
 
 const {fields, validate} = buildSchema(schema)
 
-console.log('schema')
-console.log(schema)
-
-let frm = (schema, fields) => {
-  return {
-    Field: class Field extends Component {
-      render () {
-        console.log('in field')
-        console.log(fields)
-        console.log(schema)
-        return (
-        <label>Name</label>
-        )
-      }
-    }
-  }
-}
-
-const {Field} = frm(schema, fields)
-console.log('field')
-console.log(Field)
-
-class PersonForm extends FormComponent {
+class PersonForm extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired
   };
   render () {
-    const {title, fields, handleSubmit, errors} = this.props
-    console.log(errors)
-    console.log(fields.name)
     return (
       <div>
-        <h3>{title}</h3>
-        <form className='ui form' onSubmit={handleSubmit}>
-          <input type='hidden' {...fields.id} />
-
+        <h3>{this.props.title}</h3>
+        <Form {...this.props}>
           <Field name='name' />
-
-          <div className={classNames('field', {'error': fields.name.error})}>
-            <label>Name</label>
-            <input type='text' placeholder='Name' {...fields.name} />
-          </div>
-
-            <label>Location</label>
-            <div className='field'>
-              <input type='text' placeholder='Los Angeles, CA' {...fields.location} />
-            </div>
-            <label>Images</label>
-            <div className='field'>
-              <input type='text' placeholder='http://me.com/small.png' {...fields.smallImage} />
-            </div>
-            <div className='field'>
-              <input type='text' placeholder='http://me.com/large.png' {...fields.largeImage} />
-            </div>
+          <Field name='location' />
           <div className='field'>
-            <label>Social</label>
-            <div className='six wide field'>
-              <input type='text' placeholder='Email' {...fields.email} />
-            </div>
-            <div className='six wide field'>
-              <input type='text' placeholder='Twitter' {...fields.twitter} />
-            </div>
-            <div className='six wide field'>
-              <input type='text' placeholder='Facebook' {...fields.facebook} />
-            </div>
-            <div className='six wide field'>
-              <input type='text' placeholder='Instagram' {...fields.instagram} />
-            </div>
+            <label>Images</label>
+            <Field name='smallImage' noLabel />
+            <Field name='largeImage' noLabel />
           </div>
-          <button className='ui button' onClick={handleSubmit}>Submit</button>
-        </form>
+          <div className='six wide field'>
+            <Field name='email' />
+            <Field name='twitter' />
+            <Field name='facebook' />
+            <Field name='instagram' />
+          </div>
+          <button className='ui button' onClick={this.props.handleSubmit}>Submit</button>
+        </Form>
       </div>
     )
   }
 }
 
-export default Form({
+export default shastaForm({
   name: 'person',
   fields: fields,
   validate: validate
--- local_modules/shasta-forms/index.js
+++ local_modules/shasta-forms/index.js
@@ -1,25 +1,118 @@
+import React from 'react'
+import jif from 'jif'
 import { fromJS } from 'immutable'
 import { reducer as formReducer, reduxForm } from 'redux-form'
 import { Component, PropTypes } from 'shasta'
+import classNames from 'classnames'
+import startCase from 'lodash.startcase'
 
-export const reducer = (state, action) =>
-  fromJS(formReducer(state ? state.toJS() : {}, action))
+/*
+  Form component
+  redux-form form container that abstracts need for defining required props
+  and provides props to child Field elements
 
-export class FormComponent extends Component {
+  * noLabel: include (set to true) in tag if you want to not have a lable
+  * label: set a label explicitly
+*/
+
+export class Form extends Component {
   static propTypes = {
     fields: PropTypes.object.isRequired,
     handleSubmit: PropTypes.func.isRequired,
     resetForm: PropTypes.func.isRequired,
     submitting: PropTypes.bool.isRequired,
-    validate: PropTypes.func.isRequired
+    validate: PropTypes.func.isRequired,
+    children: PropTypes.node
   };
+  static childContextTypes = {
+    fields: React.PropTypes.object
+  };
+  getChildContext () {
+    return {fields: this.props.fields}
+  }
+  render () {
+    return (
+      <form className='ui form' onSubmit={this.props.handleSubmit}>
+        {this.props.children}
+      </form>
+    )
+  }
 }
 
-export const getValues = reduxForm.getValues
+/*
+  # Field component
+  wraps label, input and error handling into one component
+  options are loaded from schema into parent form
 
-export const Form = (opt = {}) =>
+  * name: name of the field, used for error handling, redux-form integration and auto-label
+  * noLabel: include (set to true) in tag if you want to not have a lable
+  * label: set a label explicitly
+*/
+
+export class Field extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    noLabel: PropTypes.boolean,
+    label: PropTypes.string
+  };
+  static contextTypes = {
+    fields: React.PropTypes.object
+  };
+  static defaultProps = {
+    type: 'text'
+  };
+  render () {
+    let field = this.context.fields[this.props.name]
+    console.log('field')
+    console.log(this.context.fields)
+    console.log(this.props.name)
+    console.log(field)
+    let label = this.props.label || startCase(this.props.name)
+    return (
+      // mixin error class if there's an error */}
+      <div className={classNames('field', {'error': field.error})}>
+        {/* noLabel */}
+        {(this.props.noLabel) ? null : <label>{label}</label>}
+        {/* acutal input */}
+        <input type={this.props.type} {...field} />
+        {
+          jif(field.error, () =>
+            <div
+              className='ui basic red pointing prompt label transition visible'>
+              {field.error}
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+}
+
+/*
+  shastaForm decorator
+  wraps redux-form decorator and provides some default values
+
+  * reduxMountPoint: where in the store forms should be mounted- default is 'form', we default to 'forms'
+  * form: opt.name - name is an alias for 'form', simply the name of the form
+  * getFormState: convert to JS for redux-form to work with immutable.js
+*/
+
+export const shastaForm = (opt = {}) =>
   reduxForm({
     reduxMountPoint: 'forms',
+    form: opt.name,
     getFormState: (state, cursor) => state.get(cursor).toJS(),
     ...opt
   })
+
+/*
+  reducer
+  redux-form reducer wrapped to support immutable.js
+*/
+
+export const reducer = (state, action) =>
+  fromJS(formReducer(state ? state.toJS() : {}, action))
+
+// export redux-form's getValues
+export const getValues = reduxForm.getValues
